ed01426983c65e56ab117c563e0c984f3527ad07
DESKTOP-QM8FO1D\STL <nedcrow0@gmail.com>
2020-09-23 18:00:16 +0900

    Cleanup

---
+++ Source/LOBG/Lobby/UI/ChattingWidgetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChattingWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOBG_API UChattingWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LOBG/Lobby/UI/LobbyWidgetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LobbyWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOBG_API ULobbyWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
