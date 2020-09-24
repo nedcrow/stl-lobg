cd3fb26a24fede80798f4dd8f0306ab02c86bcc0
SBA_OJW <limeleafgg@gmail.com>
2020-09-24 17:31:23 +0900

    이미시브 총알. 프로젝타일 채널.

---
+++ Source/LOBG/Weapon/EmissiveBullet.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EmissiveBullet.generated.h"
+
+UCLASS()
+class LOBG_API AEmissiveBullet : public AActor
+{
+	GENERATED_BODY()
+	
+
+	/** Sphere collision component */
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+		class USphereComponent* CollisionComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Projectile, meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* StaticMesh;
+
+	/** Projectile movement component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+		class UProjectileMovementComponent* ProjectileMovement;
+
+public:
+	// Sets default values for this actor's properties
+	AEmissiveBullet();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	/** called when projectile hits something */
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+};
