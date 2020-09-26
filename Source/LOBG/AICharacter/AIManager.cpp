253c11ca581d1d324d4ea8daa675fdd7f174f717
SBA_OJW <limeleafgg@gmail.com>
2020-09-26 23:46:23 +0900

    오정원 토요일. AI매니저 정렬함수 수정.

--- Source/LOBG/AICharacter/AIManager.cpp
+++ Source/LOBG/AICharacter/AIManager.cpp
@@ -57,9 +57,19 @@ void AAIManager::SeachCoursePoints()
 				{
 					CoursePoints.Add(CurrentElement);
 				}
-				else if (CurrentElement->CourseNumber < CoursePoints[ArrayIndex]->CourseNumber)		// 마지막 원소보다 코스넘버가 작으면 현재 배열 번호에 삽입.
+				else 	// 마지막 원소보다 코스넘버가 작으면 앞쪽 배열을 검색하여 코스넘버를 비교하여 삽입할 위치를 찾는다.
 				{
-					CoursePoints.Insert(CurrentElement, ArrayIndex);
+					while (true)
+					{
+						ArrayIndex--;
+						if (ArrayIndex <= 0			// 배열의 가장 앞일 경우. 이미 이전 루프에서 조건 검사를 했기 때문에 바로 삽입한다.
+							|| (int)CoursePoints[ArrayIndex - 1]->WaveCourse != i			// 한칸 앞 원소가 다른 코스일 경우
+							|| CurrentElement->CourseNumber >= CoursePoints[ArrayIndex - 1]->CourseNumber)		// 한칸 앞 원소의 코스넘버가 작을 경우
+						{
+							CoursePoints.Insert(CurrentElement, ArrayIndex);
+							break;
+						}
+					}
 				}
 
 				OutActors.RemoveAt(j--);		// 배열에서 추가한 원소를 제거.
@@ -99,6 +109,14 @@ void AAIManager::SpawnMinions(int MinionQuantity)
 	{
 		return;
 	}
+
+	UE_LOG(LogClass, Warning, TEXT("-------CoursePoints.Num()-------- %d"), CoursePoints.Num());
+	for (int i = 0; i < CoursePoints.Num(); i++)
+	{
+		UE_LOG(LogClass, Warning, TEXT("-----WaveCourse------- %d"), CoursePoints[i]->WaveCourse);
+		UE_LOG(LogClass, Warning, TEXT("----CourseNumber------ %d"), CoursePoints[i]->CourseNumber);
+	}
+
 
 	// 스폰 포인트 찾기
 	FVector SpawnLocation = CoursePoints[0]->GetActorLocation();
