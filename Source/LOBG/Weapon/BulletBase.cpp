4c674f4d5b142383697a4754152f5c473f61ff20
SBA_OJW <limeleafgg@gmail.com>
2020-10-18 17:58:38 +0900

    미니언 피격시 공격하도록 수정

--- Source/LOBG/Weapon/BulletBase.cpp
+++ Source/LOBG/Weapon/BulletBase.cpp
@@ -66,8 +66,8 @@ void ABulletBase::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent,
 	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 	bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogClass, Warning, TEXT("OnBeginOverlap OtherComp  : %s"), *OtherComp->GetName());
-	
+	UE_LOG(LogClass, Warning, TEXT("Other Actor : %s %s"), *OtherActor->GetName(), *SweepResult.BoneName.ToString());
+
 
 
 	// 같은팀이라면 return
